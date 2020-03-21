c063db2c4517abdcfbdd9f98e4597241beb2c362
Mojeedkush <mkusimo90@gmail.com>
2020-03-21 17:00:41 +0100

    First Js commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let student = "Mojeed Kusimo";
+let courses = ["HTML", "CSS", "Javascript", "Node.js"];
+
+console.log(student);
+console.log(courses);
+
+for (let i = 0; i <= 200; i++) {
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+}
